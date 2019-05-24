d3bb6f0d4520111de1b7d8c057d73009f2069616
Munieru <20086673+munierujp@users.noreply.github.com>
2019-05-25 02:17:03 +0900

    Fix bug when called multiple times

--- docs/widget.js
+++ docs/widget.js
@@ -1,135 +1,112 @@
 (function () {
   const CLASS_NAME_TARGET = 'jp-munieru-hatenablog-embed-widget'
-  const CLASS_NAME_WIDGET = 'jp-munieru-hatenablog-embed-widget-iframe'
+  const WIDGET_ELEMENT_ATTRIBUTES = {
+    scrolling: 'no',
+    frameBorder: 0,
+    marginWidth: 0,
+    marginHeight: 0
+  }
+  const WIDGET_ELEMENT_STYLES = {
+    display: 'block',
+    width: '100%',
+    height: '155px',
+    maxWidth: '500px',
+    margin: '10px 0px'
+  }
 
   Array.from(document.getElementsByClassName(CLASS_NAME_TARGET))
     .forEach(targetElement => {
-      const widgetElement = insertWidgetElement(targetElement)
-      widgetElement.addEventListener('load', () => {
-        updatetWidgetElement(widgetElement)
-      })
-      hideElement(targetElement)
+      const widgetElement = toWidgetElement(targetElement)
+      targetElement.parentNode.insertBefore(widgetElement, targetElement)
+      targetElement.remove()
     })
 
-  function insertWidgetElement (targetElement) {
-    const widgetElement = createWidgetElement(targetElement)
-    targetElement.parentNode.insertBefore(widgetElement, targetElement)
-    const doc = widgetElement.contentWindow.document
-    doc.open()
-    const content = createWidgetElementContent(targetElement)
-    doc.write(content)
-    doc.close()
-    return widgetElement
-  }
-
-  function createWidgetElement (targetElement) {
+  function toWidgetElement (targetElement) {
     const widgetElement = document.createElement('iframe')
-    const attributes = {
-      className: CLASS_NAME_WIDGET,
-      scrolling: 'no',
-      frameBorder: 0,
-      marginWidth: 0,
-      marginHeight: 0
-    }
-    Object.keys(attributes).forEach(key => {
-      widgetElement[key] = attributes[key]
+    Object.keys(WIDGET_ELEMENT_ATTRIBUTES).forEach(key => {
+      widgetElement[key] = WIDGET_ELEMENT_ATTRIBUTES[key]
     })
-    widgetElement.style.display = 'block'
-    widgetElement.style.width = '100%'
-    widgetElement.style.height = '155px'
-    widgetElement.style.maxWidth = '500px'
-    widgetElement.style.margin = '10px 0px'
+    Object.keys(WIDGET_ELEMENT_STYLES).forEach(key => {
+      widgetElement.style[key] = WIDGET_ELEMENT_STYLES[key]
+    })
+    widgetElement.srcdoc = createWidgetElementHTML(targetElement)
     return widgetElement
   }
 
-  function createWidgetElementContent (targetElement) {
-    const { title, url, image, body } = targetElement.dataset
-    const encodedURL = encodeURIComponent(url)
-    const host = new URL(url).host
+  function createWidgetElementHTML (targetElement) {
+    const { url, title, body, image } = targetElement.dataset
+    const headElement = createHeadElementHTML(url)
+    const bodyElement = createBodyElementHTML(url, title, body, image)
+    return `
+    <html>
+    ${headElement}
+    ${bodyElement}
+    </html>`
+  }
+
+  function createHeadElementHTML (url) {
+    return `
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="initial-scale=1.0">
+      <meta name="robots" content="noindex,follow">
+      <link rel="canonical" href="${url}">
+      <link rel="stylesheet" type="text/css" href="https://cdn.blog.st-hatena.com/css/embed.css?version=516683be4e773d4dcbb4b024a1f93070e1a4afde&amp;env=production">
+    </head>`
+  }
+
+  function createBodyElementHTML (url, title, body, image) {
+    const embedContentElement = createEmbedContentElementHTML(url, title, body, image)
+    const embedFooterElement = createEmbedFooterElementHTML(url)
+    return `
+    <body class="body-iframe page-embed hatena-web-card">
+      <div class="embed-wrapper">
+        <div class="embed-wrapper-inner">
+          ${embedContentElement}
+          ${embedFooterElement}
+        </div>
+      </div>
+    </body>`
+  }
+
+  function createEmbedContentElementHTML (url, title, body, image) {
+    const entryBodyElement = createEntryBodyElementHTML(url, title, body)
     if (image) {
       return `
-      <html>
-      <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="initial-scale=1.0">
-        <meta name="robots" content="noindex,follow">
-        <link rel="canonical" href="${url}">
-        <link rel="stylesheet" type="text/css" href="https://cdn.blog.st-hatena.com/css/embed.css?version=516683be4e773d4dcbb4b024a1f93070e1a4afde&amp;env=production">
-      </head>
-      <body class="body-iframe page-embed hatena-web-card">
-        <div class="embed-wrapper">
-          <div class="embed-wrapper-inner">
-            <div class="embed-content with-thumb">
-              <div class="thumb-wrapper">
-                <a href="${url}" target="_blank">
-                  <img src="${image}" class="thumb">
-                </a>
-              </div>
-              <div class="entry-body">
-                <h2 class="entry-title">
-                  <a href="${url}" target="_blank">${title}</a>
-                </h2>
-                <div class="entry-content">
-                  ${body}
-                </div>
-              </div>
-            </div>
-            <div class="embed-footer">
-              <a href="${url}" target="_blank"><img src="https://cdn-ak.favicon.st-hatena.com?url=${encodedURL}" alt="${host}"
-                  title="${host}" class="favicon"> ${host}</a>
-              <img src="https://s.st-hatena.com/entry.count.image?uri=${encodedURL}" alt="" class="star-count">
-              <a href="http://b.hatena.ne.jp/entry/${url}" target="_blank"><img src="https://b.hatena.ne.jp/entry/image/${url}" class="bookmark-count"></a>
-            </div>
-          </div>
+      <div class="embed-content with-thumb">
+        <div class="thumb-wrapper">
+          <a href="${url}" target="_blank">
+            <img src="${image}" class="thumb">
+          </a>
         </div>
-      </body>
-      </html>`
+        ${entryBodyElement}
+      </div>`
     } else {
       return `
-      <html>
-      <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="initial-scale=1.0">
-        <meta name="robots" content="noindex,follow">
-        <link rel="canonical" href="${url}">
-        <link rel="stylesheet" type="text/css" href="https://cdn.blog.st-hatena.com/css/embed.css?version=516683be4e773d4dcbb4b024a1f93070e1a4afde&amp;env=production">
-      </head>
-      <body class="body-iframe page-embed hatena-web-card">
-        <div class="embed-wrapper">
-          <div class="embed-wrapper-inner">
-            <div class="embed-content ">
-              <div class="entry-body">
-                <h2 class="entry-title">
-                  <a href="${url}" target="_blank">${title}</a>
-                </h2>
-                <div class="entry-content">
-                  ${body}
-                </div>
-              </div>
-            </div>
-            <div class="embed-footer">
-              <a href="${url}" target="_blank"><img src="https://cdn-ak.favicon.st-hatena.com?url=${encodedURL}" alt="${host}"
-                  title="${host}" class="favicon"> ${host}</a>
-              <img src="https://s.st-hatena.com/entry.count.image?uri=${encodedURL}" alt="" class="star-count">
-              <a href="http://b.hatena.ne.jp/entry/${url}" target="_blank"><img src="https://b.hatena.ne.jp/entry/image/${url}" class="bookmark-count"></a>
-            </div>
-          </div>
-        </div>
-      </body>
-      </html>`
+      <div class="embed-content ">
+        ${entryBodyElement}
+      </div>`
     }
   }
 
-  function updatetWidgetElement (widgetElement) {
-    const doc = widgetElement.contentWindow.document
-    const height = doc.body.scrollHeight
-    widgetElement.style.height = `${height}px`
-    doc.querySelectorAll('a').forEach(a => {
-      a.target = '_blank'
-    })
+  function createEntryBodyElementHTML (url, title, body) {
+    return `
+    <div class="entry-body">
+      <h2 class="entry-title">
+        <a href="${url}" target="_blank">${title}</a>
+      </h2>
+      <div class="entry-content">${body}</div>
+    </div>`
   }
 
-  function hideElement (element) {
-    element.style.display = 'none'
+  function createEmbedFooterElementHTML (url) {
+    const encodedURL = encodeURIComponent(url)
+    const host = new URL(url).host
+    return `
+    <div class="embed-footer">
+      <a href="${url}" target="_blank"><img src="https://cdn-ak.favicon.st-hatena.com?url=${encodedURL}" alt="${host}" title="${host}" class="favicon"> ${host}</a>
+      <img src="https://s.st-hatena.com/entry.count.image?uri=${encodedURL}" alt="" class="star-count">
+      <a href="http://b.hatena.ne.jp/entry/${url}" target="_blank"><img src="https://b.hatena.ne.jp/entry/image/${url}" class="bookmark-count"></a>
+    </div>`
   }
 })()
